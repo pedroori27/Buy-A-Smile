26b5d776adf397f7580401a302b2d712a5f8661a
pedroori27 <pedroori27@gmail.com>
2026-06-24 19:05:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let spawn = 7.5
 const myAudio = new Audio('music/mayday.wav');
 const btnPlay = document.getElementById("startbut");
-myAudio.volume = 0.3;
+myAudio.volume = 0.1;
 // Função para tocar o áudio
 btnPlay.addEventListener("click", () => {
     myAudio.play();
+    document.querySelectorAll(".teladeentrada").forEach(el => el.remove());
 });
